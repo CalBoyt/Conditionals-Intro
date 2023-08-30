1ef4a40475e5fa5751d6456acce4a36cd39d1f61
Caleen Boyter <caleenboyter@gmail.com>
2023-08-30 14:01:12 -0400

    I'm awesome and finished Conditionals

---
+++ main.js
@@ -0,0 +1,48 @@
+if(1+1 > 5){
+    console.log("True fact!");
+  } else {
+    console.log("Lies!")
+  }
+  if(4*5 <= 20){
+    console.log("True fact!");
+  } else {
+    console.log("Lies!")
+  }
+  if(6-2 >= 0){
+    console.log("True fact!");
+  } else {
+    console.log("Lies!")
+  }
+
+let likesDogs = true
+if(likesDogs = true){
+    console.log("You're a dog person!");
+} else {
+    console.log("You're a cat person!");
+}
+
+const caleen = {
+    name: "Caleen",
+    language: "Spanish"
+}
+
+if(caleen.language === "English"){
+    console.log(`Hello, ${caleen.name}!`);
+} else if (caleen.language === "Spanish"){
+        console.log(`Hola, ${caleen.name}!`);
+} else if (caleen.language === "French"){
+    console.log(`Bonjour, ${caleen.name}!`);}
+
+ 
+const grade = 97
+if (grade >= 0 && grade <= 69){
+    console.log("You got an F");
+} else if (grade >= 70 && grade <= 76){
+    console.log("You got a D");
+} else if (grade >= 77 && grade <= 84){
+    console.log("You got a C");
+} else if (grade >= 85 && grade <= 92){
+    console.log("You got a B");
+} else if (grade >= 93 && grade <= 100){
+    console.log("You got an A");
+}
